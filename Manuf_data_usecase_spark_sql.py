c0ff5090ab05206e7c5e8211558f27c9111f6f79
NaveenSubramani7 <108247157+NaveenSubramani7@users.noreply.github.com>
2022-08-27 09:57:43 +0530

    Update and rename Manufacturing_data_usecase.py to Manuf_data_usecase_spark_sql.py

--- Manuf_data_usecase_spark_sql.py
+++ Manuf_data_usecase_spark_sql.py
@@ -3,17 +3,21 @@ from pyspark.sql.functions import *
 
 spark = SparkSession.builder.master("local[*]").appName("ManufactureData").getOrCreate()
 # checked the manf_data.csv data and observed the below points.
-# found a malformed data available in the data, so planning to use 'mode' as 'dropmalformed'
+# found a malformed data available in the data, so planning to do Data Munging activity and use 'mode' as 'dropmalformed'
 # header info was available, so planning to use 'header' as 'true'
 # found two duplicate cols like prod_id & prodid and station_id twice, so planning to rename the cols with customized name using toDF
 # Get the plain Data from the source
+# Data Munging and cleaning process
+
 basicdf = spark.read.csv("file:///home/hduser/sparkdata/manuf_data.csv",sep=",",header="true",inferSchema="true",mode="dropmalformed")\
           .toDF("timestamp","prod_id1","station_id1","sensor","value","station_id2","prod_id2","part_id")
+
 # print(basicdf.count()) # uncomment this line, to verify the eliminated malformed data
 # basicdf.show(8,truncate=False) # uncomment this line, to verify the data after the malformed data is removed
 
-# Data Munging and cleaning process
 basicdf.createOrReplaceTempView("manf_view")
+
+# Data Transformation Process
 
 # Convert the timestamp column to regular Date col as recorded_date
 # Merge the prod_id1 and prod_id2 to get product_id
@@ -24,6 +28,9 @@ scrub_df = spark.sql("select CAST(timestamp AS DATE) as recorded_date, "
                      "NVL(station_id1, station_id2) as station_id, "
                      "CASE WHEN lower(sensor) = 'status' THEN '' else sensor END AS sensor,"
                      "value, part_id from manf_view")
+
+# Data Enrichment Process
+
 # As per requirement, cleaned data of the status, we need to make it as a column and hold the value of the status as value in the new column
 # As the status column is added, the value of the status should be removed from its record too.
 cur_df=scrub_df.withColumn("status", when(scrub_df.sensor != '', '').otherwise(scrub_df.value))\
